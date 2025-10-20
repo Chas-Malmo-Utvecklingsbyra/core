3d22946d28fbe4f7a446dbd1fee10af9b46d3d6d
Henrik Westerlund <henrik.w93@gmail.com>
2025-10-20 16:33:31 +0200

    Finished moving old json functions from old project and added some comments

--- json/fileHelper/fileHelper.c
+++ json/fileHelper/fileHelper.c
@@ -5,11 +5,11 @@
 
 /* Creates and returns a cJSON pointer that contains
 the data from the parsed JSON file */
-cJSON* Read_JSON_From_File(const char* fileName)
+cJSON* Read_JSON_From_File(const char* _FileName)
 {
     /* Opens file in read mode and 
     ensures that it was successful */
-    FILE *file = fopen(fileName, "r");
+    FILE *file = fopen(_FileName, "r");
     if (file == NULL)
     {
         return NULL;
@@ -29,8 +29,8 @@ cJSON* Read_JSON_From_File(const char* fileName)
         return NULL;
     }
 
-    /* Transfers contents of the JSON file and stores it
-    in the JsonString buffer, then parses the data using cJSON */
+    /* Transfers contents of the JSON file to the
+    JsonString buffer, then parses the data using cJSON */
     fread(JsonString, 1, length, file);
     fclose(file);
 
@@ -48,3 +48,73 @@ cJSON* Read_JSON_From_File(const char* fileName)
     return root;
 }
 
+/* Returns 0 for success and -1 for failure */
+int Write_Json_To_File(const char* _FileName, cJSON* _JSON_Object)
+{
+    /* Opens file in write mode and uses cJSON_Print to
+    create valid JSON data that is then printed into file */
+    assert(_JSON_Object != NULL);
+    FILE *f = fopen(_FileName, "w");
+    if (f == NULL)
+    {
+        printf("Error opening file\n");
+        return -1;
+    }
+    char* tempJsonStr = cJSON_Print(_JSON_Object);
+
+    fprintf(f, "%s\n", tempJsonStr);
+    fclose(f);
+    free(tempJsonStr);
+    return 0;
+}
+
+/* Attempts to open file in order to determine if it already exists */
+int DoesFileExist(const char* _FileName)
+{
+    FILE *file = fopen(_FileName, "r");
+    if (file)
+    {
+        fclose(file);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* Same as Read_JSON_From_File except it returns a 
+char* instead of a cJSON pointer */
+char* Read_JSON_From_File_Return_Char(const char* _FileName)
+{
+    FILE *file = fopen(_FileName, "r");
+    if (file == NULL)
+    {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    rewind(file);
+
+    char* JsonString = calloc(1, length + 1);
+    if (JsonString == NULL)
+    {
+        fclose(file);
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+    fread(JsonString, 1, length, file);
+    fclose(file);
+    JsonString[length] = '\0';
+    return JsonString;
+}
+
+int DeleteFile(const char* _FileName)
+{
+    if (remove(_FileName) == 0)
+    {
+        return 0;
+    } else {
+        perror("Failed to delete file");
+        return -1;
+    }
+}
--- json/fileHelper/fileHelper.h
+++ json/fileHelper/fileHelper.h
@@ -3,6 +3,14 @@
 
 #include "../cJSON/cJSON.h"
 
-cJSON* Read_JSON_From_File(const char* fileName);
+cJSON* Read_JSON_From_File(const char* _FileName);
+
+int Write_Json_To_File(const char* _FileName, cJSON* JSON_Object);
+
+char* Read_JSON_From_File_Return(const char* _FileName);
+
+int DoesFileExist(const char* _FileName);
+
+int DeleteFile(_FileName);
 
 #endif
