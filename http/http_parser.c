0137ce4faada6db3d9922bfc6601597d66e901f9
pontusrosenquistgmailcom <pontus.rosenquist@gmail.com>
2025-10-24 20:49:47 +0200

    http_parser updated c and h file. now has http_destroy to clean up HttpRequestBlob struct

--- http/http_parser.c
+++ http/http_parser.c
@@ -3,25 +3,21 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-/* (PR) The stuff we created last wednesday(?), start of basic http parser */
+#include "http_parser.h"
 
-/* save parsed data into HTTPREQUESTBLOB and HEADER */
-typedef struct Header {
-    char* keyName; 
-    char* KeyValue;
-    struct Header *next;
-} Header;
+/*
+EXAMPLE OF HTTP TEXT:
 
-typedef struct HttpRequestBlob {
-    char* method;
-    char* path;
-    char* version;
-    Header *Headers;
-    char* body;
-} HttpRequestBlob;
+GET /weather?city=Stockholm HTTP/1.1\r\n
+Host: 127.0.0.1\r\n
+User-Agent: curl/8.0\r\n
+Content-Length: 9\r\n\r\n
+BODY TEXT\r\n
 
+TO HELP MAKE PARSER LOGIC.
+*/
 
-int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer)
+int http_parse(const char* _InBuffer, size_t _InBufferSize, HttpRequestBlob* _HttpBlob)
 {
     HttpRequestBlob* httpData = (HttpRequestBlob*)malloc(sizeof(HttpRequestBlob));
     memset(httpData, 0, sizeof(httpData));
@@ -31,32 +27,25 @@ int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer)
 
     char* pch; 
 
-    /*
-    EXAMPLE OF HTTP TEXT:
+    char* meth = NULL;
+    char* path = NULL;
+    char* vers = NULL;
 
-    GET /weather?city=Stockholm HTTP/1.1\r\n
-    Host: 127.0.0.1\r\n
-    User-Agent: curl/8.0\r\n
-    \r\n
-
-    TO HELP MAKE PARSER LOGIC.
-    */
-
+    
     pch = strtok(dataCopy, "\r\n");
-
     while (pch != NULL)
     {
-        /*printf("%s\n", pch);*/
         ptr = pch;
         do{
             if(httpData->method != NULL && httpData->path != NULL && httpData->version != NULL){
+                /* Identify headers and parse KEY and VALUE */
                 char* keyPart = strchr(pch, ':');
                 if(keyPart) {
                     *keyPart = '\0';
                     char* keyName = pch;
                     char* valueName = keyPart +1;
                     
-                    while(*valueName == ' ') /* remove prefix white space */
+                    while(*valueName == ' ')
                     valueName++;
                     
                     Header* newHeader = (Header*)malloc(sizeof(Header));
@@ -79,26 +68,32 @@ int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer)
                 }
             }
 
-            if(httpData->method == NULL)
+            /* obtain request data METHOD PATH VERSION */
+            if(meth == NULL)
             {
-                httpData->method = ptr;
+                meth = ptr;
             }
-            else if(httpData->path == NULL)
+            else if(path == NULL)
             {
                 if(*(ptr) == ' ')
                 {
-                    httpData->path = ptr +1;
+                    path = ptr +1;
                     *(ptr) = '\0';
                 }
             }
-            else if(httpData->version == NULL)
+            else if(vers == NULL)
             {
                 if(*(ptr) == ' '){
-                    httpData->version = ptr +1;
+                    vers = ptr +1;
                     *(ptr) = '\0';
+                    /* i want to str dup these */
+                    httpData->method = strdup(meth);
+                    httpData->path = strdup(path);
+                    httpData->version = strdup(vers);
                     break;
                 }
             }
+           
             ptr++;
         }while (*(ptr) != '\0');
 
@@ -113,14 +108,10 @@ int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer)
     }    
 
 
-    ptr = dataCopy;
 
     printf("httpdata method  : %s\n", httpData->method);
     printf("httpdata path    : %s\n", httpData->path);
     printf("httpdata version : %s\n", httpData->version);
-
-/* 	struct addrinfo *rp;
-	for(rp = res; rp; rp = rp->ai_next) */
 
     struct Header* hdr;
     for(hdr = httpData->Headers; hdr != NULL; hdr = hdr->next)
@@ -131,6 +122,10 @@ int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer)
 
     printf("Http body        : %s", httpData->body);
 
+    free(dataCopy);
+    ptr = NULL;
+    http_destroyBlob(httpData);
+
     return 0;
 }
 
@@ -139,9 +134,35 @@ int http_parseBody();
 int http_parseHeader();
 
 
+int http_destroyBlob(HttpRequestBlob* _Blob){
+
+    if(_Blob == NULL)
+        return 0;
+
+    free(_Blob->method);
+    free(_Blob->path);
+    free(_Blob->version);
+    free(_Blob->body);
+
+    Header* lastHdr = _Blob->Headers;
+    while(lastHdr != NULL){
+
+        Header* next = lastHdr->next;
+        free(lastHdr->keyName);
+        free(lastHdr->KeyValue);
+        free(lastHdr);
+        lastHdr = next;
+    }
+    free(_Blob);
+
+
+    return 0;
+}
+
+
 int main(){
 
-    char* httpStr = "GET /weather?city=Stockholm HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: curl/8.0\r\nContent-Length: 9\r\n\r\nBODY TEXT\r\n";
+    const char* httpStr = "GET /weather?city=Stockholm HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: curl/8.0\r\nContent-Length: 9\r\n\r\nBODY TEXT\r\n";
 
     http_parse(httpStr, 0, NULL);
 
--- http/http_parser.h
+++ http/http_parser.h
@@ -1,10 +1,28 @@
 
 #ifndef __HTTP_PARSER_H__
 #define __HTTP_PARSER_H__
-
+#include <stddef.h>
 /* (PR) The stuff we created last wednesday(?), start of basic http parser */
 
-int http_parse(const char* _InBuffer, size_t _InBufferSize, char* _OutBuffer);
+/* save parsed data into HTTPREQUESTBLOB and HEADER */
+typedef struct Header {
+    char* keyName; 
+    char* KeyValue;
+    struct Header *next;
+} Header;
+
+typedef struct HttpRequestBlob {
+    char* method;
+    char* path;
+    char* version;
+    Header *Headers;
+    char* body;
+} HttpRequestBlob;
+
+
+int http_parse(const char* _InBuffer, size_t _InBufferSize, HttpRequestBlob* _HttpBlob);
+
+int http_destroyBlob(HttpRequestBlob* _Blob);
 
 /* NYI */
 int http_parseBody();
