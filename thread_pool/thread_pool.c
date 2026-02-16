5651a3fcab6ed38559b505887921446de34a07b8
lundhpargmailcom <lundh.par@gmail.com>
2026-02-16 15:40:26 +0100

    Updated a few bugs in thread pool, now it correctly shows if the thread is in use

--- thread_pool/thread_pool.c
+++ thread_pool/thread_pool.c
@@ -6,15 +6,11 @@ void* thread_worker(Thread_Item* thread_item)
 {
     while (1)
     {
-        
         if (thread_item == NULL || thread_item->task == NULL)
             continue;
-        
-        if(thread_item->in_use)
-            break;
 
         thread_item->result = thread_item->task(thread_item->arg);
-        thread_item->task((void*)thread_item->arg);
+        /* thread_item->task((void*)thread_item->arg); */
         thread_item->task = NULL;
         thread_item->arg = NULL;
         thread_item->in_use = false;
@@ -43,10 +39,10 @@ void Thread_Pool_Create(Thread_Pool *pool)
         Thread_Item *item = &pool->threads[i];
         item->arg = NULL;
         item->task = NULL;
-        item->in_use = true;
+        item->in_use = false;
         item->result = NULL;
         
-        pthread_create(&item->thread, NULL, callback, item->arg);
+        pthread_create(&item->thread, NULL, callback, item);
         printf("Created2\n");
     }
 
@@ -59,13 +55,12 @@ void Thread_Pool_Task_Add(Thread_Pool *pool, Handle_Thread_Function func, void *
     {
         Thread_Item *item = &pool->threads[i];
         if (!item->in_use)
-            continue;
-
-        item->task = func;
-        item->arg = arg;
-        item->in_use = true;
-        
-        break;
+        {
+            item->task = func;
+            item->arg = arg;
+            item->in_use = true;
+            break;
+        }
     }
 }
 
